bcd9090102be26e736922e852e154df5ca139f4d
Benoit Charbonnier <benoit.charbonnier@amadeus.com>
2014-04-15 14:39:42 +0200

    Early version of public website skeleton
    This commit is HUGE
    It includes all the moves related to the creation of new build
    for the documentation website, and a lot of refactoring.
    
    For example, tests are not anymore in `public` folder.

---
+++ docs/index.md
@@ -0,0 +1,72 @@
+title: Javascript Framework
+cssclass: home
+mappings:
+  MANTRA_BEGIN: <div class="mantra">
+  MANTRA_END: </div>
+  CONTENT_BEGIN: <article class="columns"><div class="column">
+  CONTENT_END: </div></article>
+---
+
+[MANTRA_BEGIN]
+## Web fuel for the long run
+### Hashspace is a powerful and lightweight JavaScript template engine that focuses on simplicity and efficiency. <br /><br />It is free and open-source.
+[MANTRA_END]
+
+[CONTENT_BEGIN]
+## Easy
+#### Syntax
+Hashspace turns static HTML pages into dynamic templates with the help of a few easy to learn statements that feel very natural to use:
+
+```html
+{if tasklist.length > 0}
+  <ul>
+  {foreach task in tasklist}
+    <li class="{'done': task.completed, 'ongoing': !task.completed}">
+      {task.description}
+    </li>
+  {/foreach}
+  </ul>
+{/if}
+```
+
+#### Errors management
+
+Templates are compiled into pure JavaScript modules, which is great because browsers don’t need extra processing to run them.
+The generation of meaningful errors is another nice feature that makes debugging straightforward:
+
+```bash
+Fatal error: HSP compilation error in src/hello.hsp (2,3): Missing end if statement
+Fatal error: HSP compilation error in src/hello.hsp (2,3): Missing end element &lt;/h1>
+```
+
+## Performant
+
+Working with templates means that, at any given time, only the necessary markup is processed and displayed on the browser: no more juggling with `display:none` and keeping useless elements active inside the DOM.
+
+## Powerful
+
+Hashspace takes care for you of the finicky plumbing required to synchronize the DOM and the data model so that you focus on what matters: designing your interface.
+
+Implicit bindings modify your model as soon as a value is changed in the user interface while, conversely, changes in the data model will automatically reflect on the display.
+And because Hashspace uses a virtual DOM for its computations, these updates are optimized and actual DOM manipulations are kept to a minimum.
+
+## Future-proof
+
+We believe in standards and best practices, so we built Hashspace with these in mind so that your code stays as much forward-compatible as possible.
+
+The engine is compatible with [CommonJS dependency management](http://noder-js.ariatemplates.com) principles and the upcoming object.observe() implementation.
+Hashspace components (have a look at the [playground](/playground/) also bring forward the much anticipated web components and make them available to every supported browser today.
+
+
+## Robust
+
+Hashspace’s code reliability is monitored by a comprehensive test suite.
+Each new feature is reviewed and checked before it is integrated.
+The people behind Hashspace have years of experience in framework design and building solid solutions, mostly thanks to the work they put in [Aria Templates](http://ariatemplates.com).
+
+[CONTENT_END]
+
+
+
+
+
---
+++ test/compiler/jsvalidator/tests.js
@@ -0,0 +1,84 @@
+// Mocha test suite
+var assert = require("assert");
+var jsv = require("../../../hsp/compiler/jsgenerator/jsvalidator/validator");
+
+describe('JS Validator: ', function(){
+
+    it ('tests a valid script', function(){
+        var s=[ 'var x="text1";',
+                'function func() {var x="text2";};',
+                'var z;'
+                ].join("\r\n");
+
+        var r=jsv.validate(s);
+
+        assert.equal(r.isValid,true,"valid result");
+        assert.equal(r.errors,null,"no errors");
+    });
+
+
+    it ('tests a simple error', function(){
+        var s=[ 'var x="text1";',
+                'function func() {var x="text2",};',
+                'var z;'
+                ].join("\r\n");
+
+        var r=jsv.validate(s);
+
+        assert.equal(r.isValid,false,"invalid result");
+        assert.equal(r.errors.length,1,"one error");
+        assert.equal(r.errors[0].description,'SyntaxError: Unexpected token',"error msg");
+        assert.equal(r.errors[0].line,2,"error line number");
+        assert.equal(r.errors[0].column,31,"error column number");
+
+        s='function func() {var x="text2",};\r\n-------------------------------^-';
+        assert.equal(r.errors[0].lineInfoTxt,s,"error line info as text");
+        s='<span class="code">function func() {var x="text2",<span class="error" title="SyntaxError: Unexpected token">}</span>;</span>';
+        assert.equal(r.errors[0].lineInfoHTML,s,"error line info as HTML");
+    });
+
+    it ('tests an error at line start', function(){
+        var s=[ 'var x="text1";',
+                'function func() {var x="text2"};',
+                '?;var z;'
+                ].join("\r\n");
+
+        var r=jsv.validate(s);
+
+        assert.equal(r.isValid,false,"invalid result");
+        assert.equal(r.errors.length,1,"one error");
+        assert.equal(r.errors[0].description,'SyntaxError: Unexpected token',"error msg");
+        assert.equal(r.errors[0].line,3,"error line number");
+        assert.equal(r.errors[0].column,0,"error column number");
+
+        s='?;var z;\r\n^-------';
+        assert.equal(r.errors[0].lineInfoTxt,s,"error line info as text");
+        s='<span class="code"><span class="error" title="SyntaxError: Unexpected token">?</span>;var z;</span>';
+        assert.equal(r.errors[0].lineInfoHTML,s,"error line info as HTML");
+    });
+
+    it ('tests an error at line start+1', function(){
+        var s=[ 'var x="text1";',
+                'function func() {var x="text2"};',
+                ' ?;var z;'
+                ].join("\r\n");
+
+        var r=jsv.validate(s);
+
+        assert.equal(r.isValid,false,"invalid result");
+        assert.equal(r.errors.length,1,"one error");
+        assert.equal(r.errors[0].description,'SyntaxError: Unexpected token',"error msg");
+        assert.equal(r.errors[0].line,3,"error line number");
+        assert.equal(r.errors[0].column,1,"error column number");
+
+        s=' ?;var z;\r\n-^-------';
+        assert.equal(r.errors[0].lineInfoTxt,s,"error line info as text");
+        s='<span class="code"> <span class="error" title="SyntaxError: Unexpected token">?</span>;var z;</span>';
+        assert.equal(r.errors[0].lineInfoHTML,s,"error line info as HTML");
+    });
+
+
+
+});
+
+
