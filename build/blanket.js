b25e0a0d8b87b48c9fb610a779007752468492ae
pk1a <pkozlowski@amadeus.com>
2014-06-05 15:03:16 +0200

    chore: restore compiler's coverage reporting

--- build/blanket.js
+++ build/blanket.js
@@ -1,11 +1,11 @@
-var path = require('path');
-var srcDir = path.join(__dirname, '..', 'hsp');
-
-require('blanket')({
-    // Only files that match the pattern will be instrumented
-    pattern: '//' + normalizeBackslashes(srcDir) + '(?!\/compiler\/parser\/hspblocks)/'
-});
-
 function normalizeBackslashes (str) {
     return str.replace(/\\/g, '/');
 }
+
+require('blanket')({
+    // Only files that match the pattern will be instrumented
+    pattern: function(fileName) {
+        fileName = normalizeBackslashes(fileName);
+        return fileName.indexOf('/hsp/compiler') >= 0 && fileName.indexOf('.peg.js') === -1;
+    }
+});
--- build/grunt/hspserver.js
+++ build/grunt/hspserver.js
@@ -1,12 +1,14 @@
 var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
 var path = require("path");
 
-var renderer = require("../../hsp/compiler/renderer");
-
 module.exports = function(grunt) {
+
     grunt.registerTask('hspserver', 'Start a web server to server compiled templates on the fly', function () {
+
+        var renderer = require("../../hsp/compiler/renderer");
+        var app = express();
+        var server = require('http').createServer(app);
+
         grunt.config.requires('hspserver.port');
         grunt.config.requires('hspserver.base');
         grunt.config.requires('hspserver.templateExtension');
